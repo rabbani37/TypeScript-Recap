02b05a7ccf6beebd45f5c34ceb75279872cc8e1f
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-06-16 09:52:00 +0600

    nulish coalescing

---
+++ src/questionMark.ts
@@ -0,0 +1,12 @@
+
+
+
+// nullish coalescing -> ??
+// question mark ------> ?.
+// Optional chaining --> ?
+const userThem = undefined;
+
+
+const setThem = userThem ?? "Light Theme"
+
+console.log(setThem);
